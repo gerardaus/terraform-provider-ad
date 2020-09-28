513e09df61acf293816cb30db59cd49115fa01f0
Gerard Paulke <Gerard.Paulke@quantium.com.au>
2020-09-27 18:23:17 -0700

    remove vendor in favor of go mod

--- ad/resource_active_directory_add_to_group_test.go
+++ ad/resource_active_directory_add_to_group_test.go
@@ -8,7 +8,7 @@ import (
 
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
-	ldap "gopkg.in/ldap.v3"
+	ldap "github.com/go-ldap/ldap/v3"
 )
 
 func TestAccAddToGroup_Basic(t *testing.T) {
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/gerardaus/terraform-provider-ad
+
+go 1.14
+
+require (
+	github.com/go-asn1-ber/asn1-ber v1.5.1
+	github.com/go-ldap/ldap/v3 v3.2.3
+	github.com/hashicorp/terraform v0.13.3
+)
